313b9ee964093dfb77578277fee6cfa4e1632895
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2018-03-08 18:07:14 +0100

    added stub for basic look

--- app/src/main/java/com/example/tholok/lab03/MainActivity.java
+++ app/src/main/java/com/example/tholok/lab03/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.tholok.lab03;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -11,9 +13,16 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        setContentView(new MyView(this));
+        //Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        MyView view = new MyView(this);
+
+        setContentView(view);
+
     }
-
 }
--- app/src/main/java/com/example/tholok/lab03/MyView.java
+++ app/src/main/java/com/example/tholok/lab03/MyView.java
@@ -3,33 +3,49 @@ package com.example.tholok.lab03;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
-/**
- * Created by tholok on 08.03.18.
- */
-
 public class MyView extends View {
 
-    Paint myPaint;
+    int ballX;
+    int ballY;
+    float ballWidth;
+
+    Paint blackPaint;
+    Paint whitePaint;
 
     public MyView(Context context) {
         super(context);
 
-        myPaint = new Paint();
+        blackPaint = new Paint();
+        whitePaint = new Paint();
+        whitePaint.setColor(0xffffffff);
 
+    }
+
+    public void init() {
+        ballX = getWidth() / 2;
+        ballY = getHeight() / 2;
+        ballWidth = getWidth() * 0.05f;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Log.d("draw", "xsize: " + getWidth());
-        Log.d("draw", "drawing");
+        init();
 
-        canvas.drawCircle(getWidth()/2, getHeight()/2, getWidth() * 0.05f, myPaint);
+        // draw bouding rect
+        canvas.drawRect(new Rect(10, 10, getWidth()-10, getHeight()-10), blackPaint);
+
+        // draw the circle
+        canvas.drawCircle(ballX, ballY, ballWidth, whitePaint);
+
+        // hacky solution (??) to continuously draw the screen
+        invalidate();
     }
 
 }
